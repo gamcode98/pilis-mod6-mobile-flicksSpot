d29b4a5deb5ae7337add75a04dbe34567f07b44c
Gabriel Alejandro Mamani <gamcode98@gmail.com>
2023-08-28 16:33:31 -0300

    add: cart logic

--- src/screens/CinemaShowDetail/CinemaShowDetailScreen.jsx
+++ src/screens/CinemaShowDetail/CinemaShowDetailScreen.jsx
@@ -1,93 +1,142 @@
 /* eslint-disable no-prototype-builtins */
-import { FlatList, Image, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { FlatList, Image, ScrollView, Text, TextInput, TouchableOpacity, View, ToastAndroid } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
-import { styles } from './CinemaShowDetailScreen.styles'
 import { useState } from 'react'
-import { DateItem } from './components/DateItem'
-import { ScheduleItem } from './components/ScheduleItem'
 import { CartIcon, IconContainer } from '../../icons'
+import { ScheduleItem, DateItem } from './components'
 import { formatHalls } from './utils/formatHalls'
+import { styles } from './CinemaShowDetailScreen.styles'
+import { SECURE_STORE_KEYS, getItem, saveItem } from '../../utils'
 
 export const CinemaShowDetailScreen = (props) => {
   const { route } = props
   const { item: { image, title, gender, description, cinemaShows } } = route.params
   const { halls } = formatHalls(cinemaShows)
-  const [selectedHall, setSelectedHall] = useState(halls[0])
-  const [availableCinemaShows, setAvailableCinemaShows] = useState(cinemaShows[`${halls[0].id}-${halls[0].name}`])
-  const [selectedDate, setSelectedDate] = useState(() => {
+
+  const defaultDate = () => {
     const hallObject = cinemaShows[`${halls[0].id}-${halls[0].name}`]
     const datesInHall = Object.keys(hallObject)
     return datesInHall[0]
+  }
+
+  const [selectedCinemaShow, setSelectedCinemaShow] = useState({
+    hall: halls[0],
+    availableCinemaShows: cinemaShows[`${halls[0].id}-${halls[0].name}`],
+    date: defaultDate(),
+    availableSchedules: cinemaShows[`${halls[0].id}-${halls[0].name}`][defaultDate()],
+    schedule: {
+      hour: cinemaShows[`${halls[0].id}-${halls[0].name}`][defaultDate()][0].hour,
+      minutes: cinemaShows[`${halls[0].id}-${halls[0].name}`][defaultDate()][0].minutes
+    },
+    totalPayment: 0
   })
-  const [availableSchedules, setAvailableSchedules] = useState(() => {
-    const hallObject = cinemaShows[`${halls[0].id}-${halls[0].name}`]
-    const datesInHall = Object.keys(hallObject)
-    const schedulesInDate = hallObject[datesInHall[0]]
-    console.log({ schedulesInDate })
-    return schedulesInDate
-  })
-  const [selectedSchedule, setSelectedSchedule] = useState(() => {
-    const hallObject = cinemaShows[`${halls[0].id}-${halls[0].name}`]
-    const datesInHall = Object.keys(hallObject)
-    const schedulesInDate = hallObject[datesInHall[0]]
-    return {
-      hour: schedulesInDate[0].hour,
-      minutes: schedulesInDate[0].minutes
-    }
-  })
-  const [totalPayment, setTotalPayment] = useState(0)
 
   const handleSelectHall = (hall) => {
-    setSelectedHall(hall)
-    setAvailableCinemaShows(cinemaShows[`${hall.id}-${hall.name}`])
-    setSelectedDate(() => {
-      const hallObject = cinemaShows[`${hall.id}-${hall.name}`]
-      const datesInHall = Object.keys(hallObject)
-      return datesInHall[0]
-    })
-    setAvailableSchedules(() => {
-      const hallObject = cinemaShows[`${hall.id}-${hall.name}`]
-      const datesInHall = Object.keys(hallObject)
-      const schedulesInDate = hallObject[datesInHall[0]]
-      return schedulesInDate
-    })
-    setSelectedSchedule(() => {
+    setSelectedCinemaShow(() => {
       const hallObject = cinemaShows[`${hall.id}-${hall.name}`]
       const datesInHall = Object.keys(hallObject)
       const schedulesInDate = hallObject[datesInHall[0]]
       return {
-        hour: schedulesInDate[0].hour,
-        minutes: schedulesInDate[0].minutes
+        hall,
+        availableCinemaShows: cinemaShows[`${hall.id}-${hall.name}`],
+        date: datesInHall[0],
+        availableSchedules: schedulesInDate,
+        schedule: {
+          hour: schedulesInDate[0].hour,
+          minutes: schedulesInDate[0].minutes
+        },
+        totalPayment: 0
       }
     })
   }
 
   const handleSelectDate = (date) => {
-    setSelectedDate(date)
-    setAvailableSchedules(() => {
-      const schedulesInDate = cinemaShows[`${selectedHall.id}-${selectedHall.name}`][date]
-      return schedulesInDate
-    })
-    setSelectedSchedule(() => {
-      const schedulesInDate = cinemaShows[`${selectedHall.id}-${selectedHall.name}`][date]
+    setSelectedCinemaShow(prevState => {
+      const hallObject = cinemaShows[`${prevState.hall.id}-${prevState.hall.name}`]
+      const schedulesInDate = hallObject[date]
       return {
-        hour: schedulesInDate[0].hour,
-        minutes: schedulesInDate[0].minutes
+        ...prevState,
+        date,
+        availableSchedules: schedulesInDate,
+        schedule: {
+          hour: schedulesInDate[0].hour,
+          minutes: schedulesInDate[0].minutes
+        },
+        totalPayment: 0
       }
     })
   }
 
   const handleSelectSchedule = (hour, minutes) => {
-    setSelectedSchedule(() => {
-      const schedulesInDate = cinemaShows[`${selectedHall.id}-${selectedHall.name}`][selectedDate]
-      const foundSchedule = schedulesInDate.find(schedule => schedule.hour === hour && schedule.minutes === minutes)
-      return foundSchedule
+    setSelectedCinemaShow(prevState => {
+      const schedulesInDate = prevState.availableSchedules
+      const foundSchedule = schedulesInDate.find(schedule => {
+        return schedule.hour === hour && schedule.minutes === minutes
+      })
+
+      return {
+        ...prevState,
+        schedule: foundSchedule,
+        totalPayment: 0
+      }
     })
   }
 
   const handleTotalPayment = (value) => {
-    const { price } = availableSchedules[0]
-    setTotalPayment(value * price)
+    const { price } = selectedCinemaShow.availableSchedules[0]
+    setSelectedCinemaShow(prevState => {
+      return {
+        ...prevState,
+        totalPayment: value * price
+      }
+    })
+  }
+
+  const addToCart = async () => {
+    const { price } = selectedCinemaShow.availableSchedules[0]
+    const { totalPayment, schedule, availableSchedules } = selectedCinemaShow
+
+    const cinemaShow = availableSchedules.find(cinemaShow => {
+      return cinemaShow.hour === schedule.hour && cinemaShow.minutes === schedule.minutes
+    })
+
+    if (totalPayment === 0) {
+      ToastAndroid.show('Debes ingresar la cantidad de boletos que deseas', ToastAndroid.SHORT)
+      return
+    }
+    const item = {
+      cinemaShowId: cinemaShow.id,
+      title,
+      image: image.url,
+      unitPrice: price,
+      quantity: totalPayment / price
+    }
+    const cart = await getItem(SECURE_STORE_KEYS.CART)
+
+    if (cart === null) {
+      const items = [item]
+      await saveItem(SECURE_STORE_KEYS.CART, JSON.stringify(items))
+      ToastAndroid.show('Agregado al carrito', ToastAndroid.SHORT)
+      return
+    }
+
+    const cartParsed = JSON.parse(cart)
+
+    const itemFound = cartParsed.find(cartItem => cartItem.cinemaShowId === item.cinemaShowId)
+
+    if (itemFound !== undefined) {
+      const indexFound = cartParsed.findIndex(cartItem => cartItem.cinemaShowId === item.cinemaShowId)
+
+      cartParsed[indexFound] = {
+        ...item,
+        quantity: itemFound.quantity + item.quantity
+      }
+      await saveItem(SECURE_STORE_KEYS.CART, JSON.stringify(cartParsed))
+      ToastAndroid.show('Agregado al carrito', ToastAndroid.SHORT)
+    } else {
+      await saveItem(SECURE_STORE_KEYS.CART, JSON.stringify([...cartParsed, item]))
+      ToastAndroid.show('Agregado al carrito', ToastAndroid.SHORT)
+    }
   }
 
   return (
@@ -130,11 +179,11 @@ export const CinemaShowDetailScreen = (props) => {
               halls.map(hall => (
                 <TouchableOpacity
                   key={hall.id}
-                  style={[styles.hall, { borderColor: selectedHall.id === hall.id ? '#F9B208' : '#000' }]}
+                  style={[styles.hall, { borderColor: selectedCinemaShow.hall.id === hall.id ? '#F9B208' : '#000' }]}
                   onPress={() => handleSelectHall(hall)}
                 >
                   <Text style={{
-                    color: selectedHall.id === hall.id ? '#F9B208' : '#000'
+                    color: selectedCinemaShow.hall.id === hall.id ? '#F9B208' : '#000'
                   }}
                   >{hall.name}
                   </Text>
@@ -143,76 +192,59 @@ export const CinemaShowDetailScreen = (props) => {
             }
           </View>
           <Text style={styles.availableDatesTitle}>Fechas disponibles:</Text>
-          <View style={{ marginBottom: 16 }}>
+          <View style={styles.availableSchedulesContainer}>
             <FlatList
-              data={Object.keys(availableCinemaShows)}
+              data={Object.keys(selectedCinemaShow.availableCinemaShows)}
               horizontal
               showsHorizontalScrollIndicator={false}
-              // snapToInterval={FULL_SIZE}
-              ItemSeparatorComponent={<Text style={{ marginHorizontal: 8 }} />}
+              ItemSeparatorComponent={<Text style={styles.itemSeparator} />}
               decelerationRate='fast'
               keyExtractor={item => item}
               renderItem={({ item }) =>
                 <DateItem
                   date={item}
-                  selectedDate={selectedDate}
+                  selectedDate={selectedCinemaShow.date}
                   handleSelectDate={handleSelectDate}
                 />}
             />
           </View>
-          <Text style={{ marginBottom: 8 }}>Horarios disponibles:</Text>
-          <View style={{ marginBottom: 16 }}>
+          <Text style={styles.availableSchedulesTitle}>Horarios disponibles:</Text>
+          <View style={styles.schedulesContainer}>
             <FlatList
-              data={availableSchedules}
+              data={selectedCinemaShow.availableSchedules}
               horizontal
               showsHorizontalScrollIndicator={false}
-              // snapToInterval={FULL_SIZE}
-              ItemSeparatorComponent={<Text style={{ marginHorizontal: 8 }} />}
+              ItemSeparatorComponent={<Text style={styles.itemSeparator} />}
               decelerationRate='fast'
               keyExtractor={item => item.id}
               renderItem={({ item: { hour, minutes } }) =>
                 <ScheduleItem
-                  selectedSchedule={selectedSchedule}
+                  selectedSchedule={selectedCinemaShow.schedule}
                   handleSelectSchedule={handleSelectSchedule}
                   hour={hour}
                   minutes={minutes}
                 />}
             />
           </View>
-          <Text style={{ marginBottom: 8 }}>Cantidad de boletos:</Text>
-          <View style={{ marginBottom: 16 }}>
+          <Text style={styles.inputPriceTitle}>Cantidad de boletos:</Text>
+          <View style={styles.containerInputPrice}>
             <TextInput
-              style={{
-                borderWidth: 1,
-                borderRadius: 8,
-                paddingVertical: 8,
-                paddingHorizontal: 16,
-                marginBottom: 8
-              }}
+              style={styles.inputPrice}
               keyboardType='numeric'
               maxLength={2}
               placeholder='Ingresa la cantidad de boletos'
               onChangeText={value => handleTotalPayment(value)}
             />
-            <Text style={{ marginBottom: 8 }}>Total a pagar: ${totalPayment}</Text>
+            <Text style={styles.totalPaymentTitle}>Total a pagar: ${selectedCinemaShow.totalPayment}</Text>
           </View>
           <TouchableOpacity
-            style={{
-              backgroundColor: '#F9B208',
-              paddingVertical: 8,
-              paddingHorizontal: 16,
-              borderRadius: 8,
-              marginBottom: 32,
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'center',
-              gap: 8
-            }}
+            style={styles.btnAddToCart}
+            onPress={addToCart}
           >
             <IconContainer color='#000'>
               <CartIcon />
             </IconContainer>
-            <Text style={{ fontWeight: '700', fontSize: 16 }}>Añadir al carrito</Text>
+            <Text style={styles.addToCartText}>Añadir al carrito</Text>
           </TouchableOpacity>
         </View>
       </View>
